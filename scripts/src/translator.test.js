3f23cb804f68367b9c348df7e23021f10f7d84f6
Lynn G <gencilynn@gmail.com>
2023-01-17 10:25:48 +1100

    minor change to test file

--- scripts/src/translator.test.js
+++ scripts/src/translator.test.js
@@ -1,79 +1,85 @@
-import { englishTextToMorseCode, morseCodeToEnglishText } from "./translator.js";
+import {
+  englishTextToMorseCode,
+  morseCodeToEnglishText,
+} from "./translator.js";
 
 const morseSequences = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "V": "...-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    "0": "-----"
-}
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+};
 
-const reverseMorseCode = Object.entries(morseSequences)
-                                .reduce( (acc, entry) => { 
-                                            acc[entry[1]] = entry[0]; 
-                                            return acc;
-                                        }, {});
+const reverseMorseCode = Object.entries(morseSequences).reduce((acc, entry) => {
+  acc[entry[1]] = entry[0];
+  return acc;
+}, {});
 
- const english = 'help';
- const morseCode = '.... . .-.. .--.';
+const english = "help";
+const morseCode = ".... . .-.. .--.";
 
-describe('englishTextToMorseCode tests', () => {
-    test('Should be defined', () => {
-        expect(englishTextToMorseCode(english, morseSequences)).toBeDefined();
-    });
+describe("englishTextToMorseCode tests", () => {
+  it("Should be defined", () => {
+    expect(englishTextToMorseCode(english, morseSequences)).toBeDefined();
+  });
 
-    test('Should translate English text to Morse Code', () => {
-        expect(englishTextToMorseCode(english, morseSequences)).toBe(morseCode);
-    });
+  it("Should translate English text to Morse Code", () => {
+    expect(englishTextToMorseCode(english, morseSequences)).toBe(morseCode);
+  });
 
-    test('Should return a ? within the result for unrecognised text characters', () => {
-        expect(englishTextToMorseCode(english + '@', morseSequences)).toBe(morseCode + ' ?');
-    });
+  it("Should return a ? within the result for unrecognised text characters", () => {
+    expect(englishTextToMorseCode(english + "@", morseSequences)).toBe(
+      morseCode + " ?"
+    );
+  });
 });
 
-describe('morseCodeToEnglishText tests', () => {
-    test('Should be defined', () => {
-        expect(morseCodeToEnglishText(morseCode, reverseMorseCode)).toBeDefined();
-    });
+describe("morseCodeToEnglishText tests", () => {
+  it("Should be defined", () => {
+    expect(morseCodeToEnglishText(morseCode, reverseMorseCode)).toBeDefined();
+  });
 
-    test('Should translate Morse Code to English text', () => {
-        expect(morseCodeToEnglishText(morseCode, reverseMorseCode)).toBe(Array.from(english.toUpperCase()).join(' '));
-    });
+  it("Should translate Morse Code to English text", () => {
+    expect(morseCodeToEnglishText(morseCode, reverseMorseCode)).toBe(
+      Array.from(english.toUpperCase()).join(" ")
+    );
+  });
 
-    test('Should return a ? within the result for invalid Morse Code', () => {
-        expect(morseCodeToEnglishText(morseCode + ' .......', reverseMorseCode)).toBe(Array.from(english.toUpperCase()).join(' ') + ' ?');
-    });
+  it("Should return a ? within the result for invalid Morse Code", () => {
+    expect(
+      morseCodeToEnglishText(morseCode + " .......", reverseMorseCode)
+    ).toBe(Array.from(english.toUpperCase()).join(" ") + " ?");
+  });
 });
-
-  
