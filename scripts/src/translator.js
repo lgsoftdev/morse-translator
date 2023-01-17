6bf7a113972000b50e7badde69ab662f679e4ee1
Lynn G <gencilynn@gmail.com>
2023-01-17 19:44:13 +1100

    use forward slash to signify space in the english text to morse code translation; get spacing of words back after translating morse code to english text

--- scripts/src/translator.js
+++ scripts/src/translator.js
@@ -1,70 +1,57 @@
 import { fetchData } from "./utils.js";
 
-export const englishTextToMorseCode = (text, mapping) => {
-    const array = Array.from(text.trim().toUpperCase()).filter(item => item !== ' ');
-    return getTranslation (array, mapping);
-}
-
-export const morseCodeToEnglishText = (code, mapping) => {
-    const array = code.trim().split(' ');
-    return getTranslation (array, mapping);
-}
-
-export const getTranslation = (array, mapping) => {
-    const fullTranslation = array.reduce((acc, item) => {
-        const itemTranslation = mapping[item];
-        
-        if (itemTranslation) {
-            return acc += itemTranslation + ' ';
-        } else {
-            return acc += '?' + ' ';
-        }
-    }, '');
-    return fullTranslation.trim();
-}
+const getTranslation = (mapping, toTranslate, delimiter, join) => {
+  const array = toTranslate.toUpperCase().split(delimiter);
+  console.log(array);
+  const translationArr = array.map((element) => {
+    return mapping[element];
+  });
+  return translationArr.join(join);
+};
 
 const encodeDecode = (mapping, translationType, toTranslate) => {
-    if (translationType == 1) return englishTextToMorseCode(toTranslate, mapping);
-    return morseCodeToEnglishText(toTranslate, mapping);  
-}
+  if (translationType == 1) {
+    return getTranslation(mapping, toTranslate, "", " ");
+  }
+  return getTranslation(mapping, toTranslate, " ", "");
+};
 
 export const getMorseSequences = async (translationType) => {
-    const morsecode = 'morsecode';
-    const mcReverse = 'morsecode-reverse';
-    let json, mapping;
+  const morsecode = "morsecode";
+  const mcReverse = "morsecode-reverse";
+  let json, mapping;
 
-    try {
-        if (!window.sessionStorage.getItem(morsecode)) {
-            json = await fetchData('./data/morse-code.json');
-            window.sessionStorage.setItem(morsecode, JSON.stringify(json));
-                
-            const jsonReverse = Object.entries(json).reduce((acc, item) => {
-                acc[item[1]] = item[0];
-                return acc;
-            }, {});
-            window.sessionStorage.setItem(mcReverse, JSON.stringify(jsonReverse));    
-        }
-    
-        if (translationType == 1) {    //translate English text to Morse Code
-            mapping = JSON.parse(window.sessionStorage.getItem(morsecode));
-        } else {    //translate Morse Code to English text
-            mapping = JSON.parse(window.sessionStorage.getItem(mcReverse));   
-        }
-    
-        return mapping;
-    } catch (error) {
-        throw error;
+  try {
+    if (!window.sessionStorage.getItem(morsecode)) {
+      json = await fetchData("./data/morse-code.json");
+      window.sessionStorage.setItem(morsecode, JSON.stringify(json));
+
+      const jsonReverse = Object.entries(json).reduce((acc, item) => {
+        acc[item[1]] = item[0];
+        return acc;
+      }, {});
+      window.sessionStorage.setItem(mcReverse, JSON.stringify(jsonReverse));
     }
-}
+
+    if (translationType == 1) {
+      //translate English text to Morse Code
+      mapping = JSON.parse(window.sessionStorage.getItem(morsecode));
+    } else {
+      //translate Morse Code to English text
+      mapping = JSON.parse(window.sessionStorage.getItem(mcReverse));
+    }
+
+    return mapping;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const translate = async (translationType, toTranslate) => {
-    try {
-        let mapping = await getMorseSequences(translationType);
-        return encodeDecode(mapping, translationType, toTranslate)
-    } catch (error) {
-        throw error;
-    }
-}
-
-
-
+  try {
+    let mapping = await getMorseSequences(translationType);
+    return encodeDecode(mapping, translationType, toTranslate);
+  } catch (error) {
+    throw error;
+  }
+};
